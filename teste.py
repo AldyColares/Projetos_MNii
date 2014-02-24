c2357568458ae8dbadb27779808161bace0bef24
CesarGACC <cesargoersch@gmail.com>
2014-02-24 12:43:12 -0200

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,2 +1,13 @@
 #teste criar arquivo
 #oi, arquivo criado
+
+def fat2(n):
+    '''
+    Entre com fat2(n) para calcular o fatorial de n
+    Exemplo: fat2(5)
+    '''
+    if (n <= 1)
+       return 1
+    return n * fat1(n-1)
+
+    print(n)
