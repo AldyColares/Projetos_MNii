ed61d845153f8a5581d302e7d4ea2f1246f6de15
AldyColares <aldycolares@gmail.com>
2014-02-24 14:30:42 -0200

    Create Regra_Trapezio.py

---
+++ Regra_Trapezio.py
@@ -0,0 +1,13 @@
+
+
+h=(b-a)/n #a é o inicio da função e b o final em relação a x. 
+
+acumulador 0,0000
+primeiro_posicao = f(x0)#f() É a função. 
+ultimo_posicao = f(xm)
+while a+h < b
+  acumulador = acumulador + f(a+h)
+  a = a+h
+
+valor_aproximado = primeira_posicao + 2*(acumulador) + ultimo_posicao
+print "valor aproximado = ", valor_aproximado 
