1eda09c01d5ff6b962cd4b92a860ccc4f63b9a77
AldyColares <aldycolares@gmail.com>
2014-02-26 19:25:58 -0200

    Update Regra_Trapezio.py

--- Regra_Trapezio.py
+++ Regra_Trapezio.py
@@ -1,35 +1,32 @@
 arquivo = open("arquivo.txt", "r")
 
-tabela = arquivo.read()
+listaStr = arquivo.readlines()
+
 arquivo.close()
-
-listaStr = tabela.readlines()
-#com quatro linhas obtenho uma lista de string com toda a tabela nela. 
-
+#com tres linhas obtenho uma lista de string com toda a tabela nela. 
 tamanhoLista = len(listaStr)
 tamanhoLista = int(tamanhoLista) 
 
-m = listaStr[0].splint[0]
-m = float(m)
-a= listaStr[2].splint[1]
+m = listaStr[0].split()[0]
+m = int(m)
+a = listaStr[2].split()[1]
 a = float(a)
-b= listaStr[tamanhoLista].splint[1]
+b = listaStr[tamanhoLista-1].split()[1]
 b = float(b)
 
 h=(b-a)/m #'a' e o inicio da funcao e 'b' o final em relacao a x da funcao. 
  
-funcao_x0 = listaStr[2].splint[2]
+funcao_x0 = listaStr[2].split()[2]
 funcao_x0 = float(funcao_x0) 
-funcao_xm = listaStr[tamanhoLista].splint[2]
+funcao_xm = listaStr[tamanhoLista-1].split()[2]
 funcao_xm = float(funcao_xm)
 
 acumulador=0.00000000
-while a+h < b
+while (a+h < b):
   i=4 #depende da posicao da tabela onde esta o f(x) do segundo ponto.
-  acumulador = acumulador + float(listaStr[i].splint[2]) 
+  acumulador = acumulador + float(listaStr[i].split()[2]) 
   a = a+h
   i = i+1  
 
 valor_aproximado = (h/2)*(funcao_x0 + 2*(acumulador) + funcao_xm)
-
 print "valor aproximado = ", valor_aproximado 
